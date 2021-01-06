2ef3a93bb977b7a6cf1bc395523d2b7c81d936bc
David Field <david@mettle.io>
2021-01-06 15:36:49 +0000

    Add route information to payload

--- src/Collector/Collector.php
+++ src/Collector/Collector.php
@@ -4,18 +4,14 @@ namespace Coremetrics\CoremetricsLaravel\Collector;
 
 class Collector
 {
-
-    /**
-     * Precision
-     */
+    /** Precision */
     const PRECISION = 2;
 
-    /**
-     * Compression keys
-     */
+    /** Compression keys */
     const COMPR_EVENT_TIMESTAMP = 't';
     const COMPR_TOTAL_DURATION = 's';
     const COMPR_PROCESS_NAME = 'n';
+    const COMPR_ROUTE_INFORMATION = 'ri';
     const COMPR_PROCESS_EVENT_BUFFER = 'l';
     const COMPR_KEY = 'k';
     const COMPR_VALUE = 'v';
@@ -23,99 +19,61 @@ class Collector
     const COMPR_META = 'm';
     const COMPR_META_TAG = 'mt';
 
-    /**
-     * @var CollectorConnectionManager
-     */
-    protected $connectionManager;
+    /** @var CollectorConnectionManager */
+    private $connectionManager;
 
-    /**
-     * @var array
-     */
-    protected $buffer = [];
+    /** @var array */
+    private $buffer = [];
 
-    /**
-     * @var integer
-     */
-    protected $lastMicrotime = LARAVEL_START;
+    /** @var float */
+    private $lastMicrotime = LARAVEL_START;
 
-    /**
-     * @var int
-     */
-    protected $totalDuration = 0;
+    /** @var float */
+    private $totalDuration = 0;
 
-    /**
-     * @var string
-     */
-    protected $processName;
+    /** @var string */
+    private $processName;
 
-    /**
-     * @param CollectorConnectionManager $connectionManager
-     */
+    /** @var array|null */
+    private $routeInformation;
+
     public function __construct(CollectorConnectionManager $connectionManager)
     {
         $this->connectionManager = $connectionManager;
     }
 
-    /**
-     * @param $processName
-     */
-    public function setProcessName($processName)
+    public function setProcessName(string $processName)
     {
         $this->processName = $processName;
     }
 
+    public function setRouteInformation(array $routeInformation)
+    {
+        $this->routeInformation = $routeInformation;
+    }
+
     /**
-     * @param $key
-     * @param $value
+     * @param string|null $key
+     * @param mixed|null $value
      * @param array $meta
+     * @param float|null $microtime
      */
-    public function prepend($key, $value, array $meta = [], $microtime = null)
+    public function prepend(string $key = null, $value = null, array $meta = [], float $microtime = null)
     {
         array_unshift($this->buffer, $this->format($key, $value, $meta, $microtime, LARAVEL_START));
     }
 
     /**
-     * @param $key
-     * @param $value
+     * @param string|null $key
+     * @param mixed|null $value
      * @param array $meta
-     * @param null $microtime
+     * @param float|null $microtime
+     *
+     * @return void
      */
-    public function append($key, $value, array $meta = [], $microtime = null)
+    public function append(string $key = null, $value = null, array $meta = [], float $microtime = null)
     {
         $this->buffer[] = $this->format($key, $value, $meta, $microtime);
-    }
-
-    /**
-     * @param $key
-     * @param $value
-     * @param array $meta
-     * @param null $now
-     * @param null $lastMicrotime
-     * @return array
-     */
-    protected function format($key, $value, array $meta = [], $now = null, $lastMicrotime = null)
-    {
-        if (!$now) {
-            $now = microtime(true);
-        }
-
-        if (!$lastMicrotime) {
-            $lastMicrotime = $this->lastMicrotime;
-        }
-
-        $diff = $now - $lastMicrotime;
-        $this->lastMicrotime = $now;
-
-        $this->totalDuration += ($diff * 1000);
-
-        return [
-            self::COMPR_KEY => $key,
-            self::COMPR_VALUE => $value,
-            self::COMPR_META => $meta,
-            self::COMPR_DURATION => round($diff * 1000, self::PRECISION)
-            // TODO(david): do we want to set the duration like this?
-            //  It seems like it might be nicer to send up the actual microtime and we can handle that on the server as we want?
-        ];
     }
 
     /**
@@ -127,11 +85,45 @@ class Collector
             self::COMPR_EVENT_TIMESTAMP => round(LARAVEL_START * 1000),
             self::COMPR_TOTAL_DURATION => round($this->totalDuration, self::PRECISION),
             self::COMPR_PROCESS_NAME => $this->processName,
+            self::COMPR_ROUTE_INFORMATION => $this->routeInformation,
             self::COMPR_PROCESS_EVENT_BUFFER => $this->buffer
         ]);
 
         $this->buffer = [];
 
         $this->connectionManager->write($json);
+    }
+
+    /**
+     * @param string|null $key
+     * @param mixed|null $value
+     * @param array $meta
+     * @param float|null $now
+     * @param float|null $lastMicrotime
+     * @return array
+     */
+    private function format(string $key = null, $value = null, array $meta = [], float $now = null, float $lastMicrotime = null): array
+    {
+        if (!$now) {
+            $now = microtime(true);
+        }
+
+        if (!$lastMicrotime) {
+            $lastMicrotime = $this->lastMicrotime;
+        }
+
+        $diff = $now - $lastMicrotime;
+
+        $this->lastMicrotime = $now;
+        $this->totalDuration += ($diff * 1000);
+
+        // TODO(david): do we want to set the duration like this?
+        //  It seems like it might be nicer to send up the actual microtime and we can handle that on the server as we want?
+        return [
+            self::COMPR_KEY => $key,
+            self::COMPR_VALUE => $value,
+            self::COMPR_META => $meta,
+            self::COMPR_DURATION => round($diff * 1000, self::PRECISION)
+        ];
     }
 }
