f5a1f59dee4c9c6296044412de4e47a9e09fc2cc
David Field <david@mettle.io>
2021-01-08 14:18:08 +0000

    Feature - Configuration Publishing (#7)

--- src/Config.php
+++ src/Config.php
@@ -30,7 +30,9 @@ class Config
      */
     public function getRemoteApiUrl(): string
     {
-        return 'http://coremetrics-server.test/metrics/c74e87e4-3633-404c-9199-553f18192c6a/application';
+        $channelToken = config('coremetrics.channel-token', 'invalid-channel-token');
+
+        return "http://coremetrics-server.test/api/metrics/{$channelToken}/application";
     }
 
     /**
